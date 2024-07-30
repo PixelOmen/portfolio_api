4a508251f265fca9b3ac6a6bb74321ccade79cf4
PixelOmen <PixelOmen@gmail.com>
2024-07-30 12:39:09 -0700

    Initial commit

---
+++ portfolio_api/portfolio_api/settings/dev.py
@@ -0,0 +1,7 @@
+from .base import *
+
+SECRET_KEY = 'django-insecure-91(gd8rvk&@%nu2*qz2v5vc2seh=d%og&wx1##wcv2c(r5t+rx'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
---
+++ portfolio_api/portfolio_api/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    re_path(r'^auth/', include('drf_social_oauth2.urls', namespace='drf'))
+]
