0139c66bed9c657d4f1a4a0566e4421fa0db22a3
PixelOmen <PixelOmen@gmail.com>
2024-08-07 16:38:51 -0700

    UserPosts model/viewset/serialiers/routes

--- portfolio_api/api/serializers.py
+++ portfolio_api/api/serializers.py
@@ -4,6 +4,10 @@ from . import models
 
 
 class UserPostSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    date_posted = serializers.DateTimeField(read_only=True)
+    owner = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = models.UserPost
-        fields = '__all__'
+        fields = ['id', 'content', 'date_posted', 'owner']
--- portfolio_api/api/views.py
+++ portfolio_api/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 
 from . import models, serializers
 
@@ -17,3 +18,25 @@ class UserPostViewSet(ModelViewSet):
     queryset = models.UserPost.objects.all()
     serializer_class = serializers.UserPostSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(owner=self.request.user)
+
+    def get_object(self):
+        obj = super().get_object()
+        if obj.owner != self.request.user:
+            raise PermissionDenied(
+                "You do not have permission to access this Post")
+        return obj
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def perform_destroy(self, instance):
+        if instance.owner != self.request.user:
+            raise PermissionDenied(
+                "You do not have permission to delete this Post")
+        instance.delete()
