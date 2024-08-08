e976b5b8e0b0096cdc946f68856fdf084275ebba
PixelOmen <PixelOmen@gmail.com>
2024-08-08 15:05:59 -0700

    added django-storages and AWS boto3 dependencies

--- portfolio_api/portfolio_api/settings/base.py
+++ portfolio_api/portfolio_api/settings/base.py
@@ -17,6 +17,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # S3 storage
+    'storages',
+
     # CORS
     'corsheaders',
 
@@ -28,6 +31,7 @@ INSTALLED_APPS = [
     'social_django',
     'drf_social_oauth2',
 
+    # Apps
     'api',
     'socialauth',
 ]
