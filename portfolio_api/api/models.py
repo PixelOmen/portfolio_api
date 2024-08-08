b4955a6a40963b31bc2568db6e2ccc5ea24cb54f
PixelOmen <PixelOmen@gmail.com>
2024-08-08 13:27:05 -0700

    Added Pillow dependency

--- portfolio_api/api/models.py
+++ portfolio_api/api/models.py
@@ -12,3 +12,14 @@ class UserPost(models.Model):
 
     def __str__(self):
         return self.id
+
+
+# class UserImage(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     image = models.ImageField(upload_to='images/')
+#     date_posted = models.DateTimeField(auto_now_add=True)
+#     owner = models.ForeignKey(
+#         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.id
