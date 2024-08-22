0c0356837250e7a1b9ca945d473001caf46ac27d
PixelOmen <PixelOmen@gmail.com>
2024-08-21 17:46:01 -0700

    Added django loggin config to dev settings

--- portfolio_api/portfolio_api/settings/dev.py
+++ portfolio_api/portfolio_api/settings/dev.py
@@ -1,3 +1,4 @@
+import sys
 from .base import *
 
 DEBUG = False
@@ -32,3 +33,45 @@ DATABASES = {
 # Celery
 CELERY_BROKER_URL = env("CELERY_BROKER_URL_DEV")
 CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND_DEV")
+
+# Logging
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "[%(asctime)s: %(levelname)s/%(processName)s] %(name)s: %(message)s",
+        },
+        "simple": {"format": "%(levelname)s %(message)s"},
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": True,
+        },
+        "celery": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": True,
+        },
+        "celery.beat": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": True,
+        },
+    },
+}
