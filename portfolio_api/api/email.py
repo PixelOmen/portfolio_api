4ec51d83c086b30827334b5e85b0861f4f0e7c7e
PixelOmen <PixelOmen@gmail.com>
2024-08-26 01:55:48 -0700

    debug: turned off fail_silently on welcome email

--- portfolio_api/api/email.py
+++ portfolio_api/api/email.py
@@ -14,8 +14,9 @@ def plain_text_message(user: str, contact_email: str, portfolio_link: str) -> st
     return f"""Hi {user},\n\nThank you for taking the time to explore the projects and demos I've put together. It means a lot to me.\n\nIf you're interested in working together, or just have any questions or feedback, please don't hesitate to contact me at {contact_email}.\n\nBest,\nEmmanuel Acosta\nPortfolio: {portfolio_link}\n\n\n\nThis is an automated message.\nYou will not receive any further emails.\nYou're receiving this email because you associated your google email with my app for the first time."""
 
 
-def ea_send_mail(subject: str, message: str,
-                 recipient_list: list[str], fail_silently=False) -> int:
+def ea_send_mail(
+    subject: str, message: str, recipient_list: list[str], fail_silently=False
+) -> int:
     """
     Basically send_mail but injects the email host user.
     """
@@ -32,9 +33,9 @@ def send_template_email(
     subject: str,
     message: str,  # plain text message if the recipient can't render HTML
     recipient_list: list[str],
-    email_template: str = 'email_template.html',
+    email_template: str = "email_template.html",
     context: dict | None = None,
-    fail_silently: bool = False
+    fail_silently: bool = False,
 ) -> int:
     """
     Send an email with an HTML template.
@@ -45,30 +46,37 @@ def send_template_email(
     html_content = render_to_string(email_template, context)
 
     msg = EmailMultiAlternatives(
-        subject, message, settings.EMAIL_HOST_USER, recipient_list)
+        subject, message, settings.EMAIL_HOST_USER, recipient_list
+    )
     msg.attach_alternative(html_content, "text/html")
 
     return msg.send(fail_silently=fail_silently)
 
 
-def send_welcome_email(username: str, email: str, html: bool = True, fail_silently: bool = True) -> int:
-    """ Send a welcome email to the user. """
+def send_welcome_email(
+    username: str, email: str, html: bool = True, fail_silently: bool = False
+) -> int:
+    """Send a welcome email to the user."""
     if html:
         return send_template_email(
             subject="Emmanuel's Portfolio - Thanks for stopping by!",
             message=plain_text_message(
-                username, settings.CONTACT_EMAIL, settings.EMAIL_PORTFOLIO_LINK),
+                username, settings.CONTACT_EMAIL, settings.EMAIL_PORTFOLIO_LINK
+            ),
             recipient_list=[email],
-            context={'user': username,
-                     'portfolio_link': settings.EMAIL_PORTFOLIO_LINK,
-                     'email_logo_url': settings.EMAIL_LOGO_URL},
-            fail_silently=fail_silently
+            context={
+                "user": username,
+                "portfolio_link": settings.EMAIL_PORTFOLIO_LINK,
+                "email_logo_url": settings.EMAIL_LOGO_URL,
+            },
+            fail_silently=fail_silently,
         )
     else:
         return ea_send_mail(
             subject="Emmanuel's Portfolio - Thanks for stopping by!",
             message=plain_text_message(
-                username, settings.CONTACT_EMAIL, settings.EMAIL_PORTFOLIO_LINK),
+                username, settings.CONTACT_EMAIL, settings.EMAIL_PORTFOLIO_LINK
+            ),
             recipient_list=[email],
-            fail_silently=fail_silently
+            fail_silently=fail_silently,
         )
